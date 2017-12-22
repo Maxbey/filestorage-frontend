4cad287e62530c8aad0aa9f0b4d46bc386bcf9a3
Maxbey <maximbeiner@gmail.com>
2017-12-22 21:00:02 +0600

    Improve UX for auth forms

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { LoginForm } from '../components/forms/LoginForm'
 
+import FlatButton from 'material-ui/FlatButton'
+
 import {Row, Col} from 'react-grid-system'
+import { Link } from 'react-router-dom'
+
+const linkContainerStyle = {
+    marginTop: '20px'
+}
 
 export class LoginView extends Component {
   render() {
@@ -9,6 +16,10 @@ export class LoginView extends Component {
       <Row style={{height: '90%'}} align="center">
         <Col offset={{md: 4.5}} style={{'padding':'10px'}} md={3}>
           <LoginForm/>
+          <FlatButton label="Create an account"
+            secondary={true} fullWidth={true} style={linkContainerStyle}
+            containerElement={<Link to='/signup/' />}
+          />
         </Col>
       </Row>
     )
--- src/views/SignupView.js
+++ src/views/SignupView.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 import { SignupForm } from '../components/forms/SignupForm'
 import {Row, Col} from 'react-grid-system'
+import { Link } from 'react-router-dom'
+import FlatButton from 'material-ui/FlatButton'
+
+const linkContainerStyle = {
+    marginTop: '20px'
+}
 
 export class SignupView extends Component {
   render() {
@@ -9,6 +15,10 @@ export class SignupView extends Component {
       <Row style={{height: '90%'}} align="center">
         <Col offset={{md: 4.5}} style={{'padding':'10px'}} md={3}>
           <SignupForm/>
+          <FlatButton label="Already have an account"
+            secondary={true} fullWidth={true} style={linkContainerStyle}
+            containerElement={<Link to='/login/' />}
+          />
         </Col>
       </Row>
     )
